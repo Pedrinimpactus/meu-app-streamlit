317e3df3c5b8342fdc3a7a0060f687529b6d9cb9
Pedro Lopes <pedro.lopes@051capital.com>
2025-04-15 14:53:01 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 17:33:32 2024
+
+@author: lrodrigues
+"""
+
+### import
+import streamlit as st
+
+
+  
+
+
+
+def main():
+    st.set_page_config(
+        page_title="Movimentação do Back",
+        page_icon=":flag-at:",
+        layout="wide"
+    )
+    
+    st.title("Rotinas Backoffice")
+    
+    st.sidebar.success("Páginas no menu lateral")
+
+
+        
+    
+    
+   # st.header("this is the markdown")
+   # st.markdown("this is the header")
+   # st.subheader("this is the subheader")
+   # st.caption("this is the caption")
+    #st.code("x=2021")
+   # st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+    pass
+
+if __name__ == '__main__':
+    main()
